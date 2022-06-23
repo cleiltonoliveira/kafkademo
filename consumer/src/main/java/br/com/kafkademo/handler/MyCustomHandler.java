9e56794dd163bfd6961d5fe17acc285efaaf08f4
Cleilton Oliveira <clltnoliveira@gmail.com>
2022-06-23 11:21:09 -0300

    Handling exceptions

---
+++ consumer/src/main/java/br/com/kafkademo/handler/MyCustomHandler.java
@@ -0,0 +1,27 @@
+package br.com.kafkademo.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyCustomHandler implements KafkaListenerErrorHandler {
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+
+        log.info("*** Inside Handler");
+        log.info("Payload {}", message.getPayload());
+        log.info("Exception {}", e.toString());
+
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+}
